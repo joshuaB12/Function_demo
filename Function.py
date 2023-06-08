1d2b9d289d2878f91fc0c6687bd6591945884ecd
joshuaB12 <136015147+joshuaB12@users.noreply.github.com>
2023-06-08 15:29:34 -0500

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# This program demonstrates a function.
+# First, we define a function named message.
+def message():
+    print('I am Arthur')
+    print('King of the Britons')
+
+
+# Call the message function.
+message()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
